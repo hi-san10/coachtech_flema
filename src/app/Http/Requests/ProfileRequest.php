b1380833e8b295d93c8abe97c2d295cf04a13b61
hi-san <h.ahkh10@icloud.com>
2025-06-29 14:45:09 +0900

    ProfileRequest修正

--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -24,7 +24,7 @@ class ProfileRequest extends FormRequest
     public function rules()
     {
         return [
-            'image' => ['required', 'mimes:jpeg, png'],
+            'image' => ['required', 'mimes:jpeg,png'],
         ];
     }
 
